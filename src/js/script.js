c53a496b5f8a23957948a7845c945b9eb4b8a30a
= <=>
2022-03-14 19:53:27 +0100

    feat: modif pages

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,16 @@
 window.addEventListener('load', initApp);
 
 function initApp() {
-    console.log("ok");
+    const lst_exos = document.querySelectorAll('.box');
+    lst_exos.forEach(elem => {
+        elem.addEventListener('click', isCheck)
+    })
+}
+
+function isCheck() {
+    if (this.classList.contains('checked')) {
+        this.classList.remove('checked');
+    } else {
+        this.classList.add('checked');
+    }
 }
