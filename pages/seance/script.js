9c61cb3a19b5a7100a335fa0d6511a8cd49c9680
= <=>
2022-03-15 18:23:20 +0100

    feat: modif pages

--- pages/seance/script.js
+++ pages/seance/script.js
@@ -1,3 +1,9 @@
 window.addEventListener('load', initApp);
 
-function initApp() {}
+function initApp() {
+    let title = document.getElementById('title');
+    let img = document.getElementById('img');
+    title.textContent = JSON.parse(localStorage.getItem('exos0')).name;
+    img.src = "../../src/img/" + JSON.parse(localStorage.getItem('exos0')).name + ".webp";
+    console.log(localStorage.length);
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,57 @@
 window.addEventListener('load', initApp);
 
 var nb_checked = 0;
+var seance = [];
 
 function initApp() {
     const lst_exos = document.querySelectorAll('.box');
-    lst_exos.forEach(elem => {
-        elem.addEventListener('click', isCheck)
+    const start_btn = document.getElementById('start');
+    lst_exos.forEach(function(elem, index) {
+        elem.addEventListener('click', function() {
+            isCheck(this, index);
+        })
     })
     document.getElementById('nb_exos').textContent = nb_checked;
+    start_btn.addEventListener('click', function(event) {
+        saveToStorage(event);
+    });
 }
 
-function isCheck() {
-    if (this.classList.contains('checked')) {
-        this.classList.remove('checked');
-        this.children[2].classList.remove('checked');
+function isCheck(elem, index) {
+    if (elem.classList.contains('checked')) {
+        elem.classList.remove('checked');
+        elem.children[2].classList.remove('checked');
         if (nb_checked > 0) {
             nb_checked -= 1;
+            seance.forEach(function(elem, i) {
+                if (elem.id === index) {
+                    seance.splice(i, 1);
+                    localStorage.removeItem('exos' + index);
+                }
+            })
         }
         document.getElementById('nb_exos').textContent = nb_checked;
     } else {
-        this.classList.add('checked');
-        this.children[2].classList.add('checked');
+        elem.classList.add('checked');
+        elem.children[2].classList.add('checked');
         nb_checked += 1;
         document.getElementById('nb_exos').textContent = nb_checked;
+        let exercice = {
+            id: index,
+            name: elem.children[1].textContent
+        };
+        seance.push(exercice);
+    }
+}
+
+function saveToStorage(event) {
+    localStorage.clear();
+    if (seance.length === 0) {
+        event.preventDefault();
+        alert('Vous devez au moins choisir un exercice.')
+    } else {
+        seance.forEach(function(elem, index) {
+            localStorage.setItem('exos' + index, JSON.stringify(elem));
+        })
     }
 }
