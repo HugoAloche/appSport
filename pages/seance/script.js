8e2a776d3e41bb21319233aabf1e4ec164b66385
= <=>
2022-03-15 18:41:23 +0100

    feat: modif pages

--- pages/seance/script.js
+++ pages/seance/script.js
@@ -1,9 +1,24 @@
 window.addEventListener('load', initApp);
 
+var i = 0;
+var current_time = document.getElementById('currentTime');
+
 function initApp() {
     let title = document.getElementById('title');
     let img = document.getElementById('img');
+    let btn_done = document.getElementById('done');
     title.textContent = JSON.parse(localStorage.getItem('exos0')).name;
     img.src = "../../src/img/" + JSON.parse(localStorage.getItem('exos0')).name + ".webp";
-    console.log(localStorage.length);
+    btn_done.addEventListener('click', startPause);
+}
+
+function startPause() {
+    setTimeout(() => {
+        document.getElementById('pause').value = i;
+        i++;
+        current_time.textContent = i + "s";
+        if (i < 31) {
+            startPause();
+        }
+    }, 1000);
 }
