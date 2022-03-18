6c5f6dff24f9645de80c203fac5e782452c2b040
Hugo Aloche <haloche035@gmail.com>
2022-03-18 17:00:44 +0100

    feat: mise ├á jour pages

--- pages/seance/script.js
+++ pages/seance/script.js
@@ -2,14 +2,21 @@ window.addEventListener('load', initApp);
 
 var i = 0;
 var current_time = document.getElementById('currentTime');
+var lst_img = [];
 
 function initApp() {
-    let title = document.getElementById('title');
-    let img = document.getElementById('img');
-    let btn_done = document.getElementById('done');
+    const title = document.getElementById('title');
+    const img = document.getElementById('img');
+    const btn_done = document.getElementById('done');
+    document.querySelectorAll('td img').forEach(elem => {
+        this.lst_img.push(elem);
+    })
     title.textContent = JSON.parse(localStorage.getItem('exos0')).name;
     img.src = "../../src/img/" + JSON.parse(localStorage.getItem('exos0')).name + ".webp";
-    btn_done.addEventListener('click', startPause);
+    btn_done.addEventListener('click', function() {
+        startPause();
+        seriesDone();
+    });
 }
 
 function startPause() {
@@ -21,4 +28,17 @@ function startPause() {
             startPause();
         }
     }, 1000);
+    document.getElementById('pause').value = 0;
+}
+
+function seriesDone() {
+    let i = 0;
+    lst_img.forEach(elem => {
+        if (elem.src.includes('close') && i < 1) {
+            elem.src = "../../src/svg/done_green.svg";
+            elem.alt = "Icône de réussite";
+            i++;
+        }
+    })
+
 }
