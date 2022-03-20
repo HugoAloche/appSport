638dff115334e8a45d6f5dd146e5f3cf0a25a693
= <=>
2022-03-20 14:39:13 +0100

    feat: modif pages

--- src/js/script.js
+++ src/js/script.js
@@ -1,57 +1,23 @@
 window.addEventListener('load', initApp);
 
-var nb_checked = 0;
-var seance = [];
-
 function initApp() {
-    const lst_exos = document.querySelectorAll('.box');
-    const start_btn = document.getElementById('start');
-    lst_exos.forEach(function(elem, index) {
-        elem.addEventListener('click', function() {
-            isCheck(this, index);
-        })
+    const form = document.getElementById('form');
+    form.addEventListener('submit', function(event) {
+        sendMail(event);
     })
-    document.getElementById('nb_exos').textContent = nb_checked;
-    start_btn.addEventListener('click', function(event) {
-        saveToStorage(event);
-    });
 }
 
-function isCheck(elem, index) {
-    if (elem.classList.contains('checked')) {
-        elem.classList.remove('checked');
-        elem.children[2].classList.remove('checked');
-        if (nb_checked > 0) {
-            nb_checked -= 1;
-            seance.forEach(function(elem, i) {
-                if (elem.id === index) {
-                    seance.splice(i, 1);
-                    localStorage.removeItem('exos' + index);
-                }
-            })
-        }
-        document.getElementById('nb_exos').textContent = nb_checked;
-    } else {
-        elem.classList.add('checked');
-        elem.children[2].classList.add('checked');
-        nb_checked += 1;
-        document.getElementById('nb_exos').textContent = nb_checked;
-        let exercice = {
-            id: index,
-            name: elem.children[1].textContent
-        };
-        seance.push(exercice);
-    }
-}
-
-function saveToStorage(event) {
-    localStorage.clear();
-    if (seance.length === 0) {
-        event.preventDefault();
-        alert('Vous devez au moins choisir un exercice.')
-    } else {
-        seance.forEach(function(elem, index) {
-            localStorage.setItem('exos' + index, JSON.stringify(elem));
-        })
-    }
+function sendMail(event) {
+    event.preventDefault();
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "haloche035@gmail.com",
+        Password: "DC6M3VYRR5TW",
+        To: "haloche035@gmail.com",
+        From: document.getElementById('email').value,
+        Subject: document.getElementById('objet').selectedIndex,
+        Body: `<html><h1>${document.getElementById('objet').selectedIndex}</h1><p>Prénom : ${document.getElementById('prenom').value} <br> Email : ${document.getElementById('email').value} <br> Message : ${document.getElementById('message').value}</p></html>`
+    }).then(
+        message => alert(message)
+    );
 }
