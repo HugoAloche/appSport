d456250fc1dcfd4d0c0b0d5a7803ebbfdd48781e
= <=>
2022-04-20 18:53:16 +0200

    feat: modif pages

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,9 +3,9 @@ export function mobileMenu() {
     const ul = document.createElement('ul');
     const link = document.createElement('a');
     link.textContent = "Accueil"
-    link.href = "/"
+    link.href = "/appSport"
     const icon = document.createElement('img');
-    icon.src = 'src/svg/home.svg';
+    icon.src = '/appSport/src/svg/home.svg';
     icon.alt = 'Icône de maison';
     const lst = navbar.appendChild(ul);
     const li1 = document.createElement('li');
@@ -17,7 +17,7 @@ export function mobileMenu() {
     link2.textContent = "Profil";
     link2.href = "/appSport/pages/profil";
     const icon2 = document.createElement('img');
-    icon2.src = 'src/svg/profile.svg';
+    icon2.src = '/appSport/src/svg/profile.svg';
     icon2.alt = 'Icône de profil';
     const li2 = document.createElement('li');
     link2.appendChild(icon2);
@@ -28,7 +28,7 @@ export function mobileMenu() {
     link3.textContent = "Paramètre"
     link3.href = "/appSport/pages/parametre"
     const icon3 = document.createElement('img');
-    icon3.src = 'src/svg/settings.svg';
+    icon3.src = '/appSport/src/svg/settings.svg';
     icon3.alt = 'Icône de paramètres';
     const li3 = document.createElement('li');
     link3.appendChild(icon3);
