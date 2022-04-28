f221fd4df65e8099dffd8195fc2bb3d8aeda0627
= <=>
2022-04-28 19:08:32 +0200

    feat: modif pages

--- pages/profil/script.js
+++ pages/profil/script.js
@@ -1,11 +1,9 @@
 import { mobileMenu } from "../../src/js/menu.js";
 import { createFooter } from "../../src/js/footer.js";
-import { createHeader } from "../../src/js/header.js";
 
 window.addEventListener('load', initApp);
 
 function initApp() {
     mobileMenu();
     createFooter();
-    createHeader();
 }
--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,9 @@
-import { createHeader } from './header.js';
 import { createFooter } from './footer.js';
 import { mobileMenu } from './menu.js';
 './footer.js';
 window.addEventListener('load', initApp);
 
 function initApp() {
-    createHeader();
     createFooter();
     mobileMenu();
 }
